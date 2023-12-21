698bcb84c1af829b497c9a4910efb602907ccc33
Sahil <sahilnegi.pang@gmail.com>
2023-12-21 12:29:18 +0530

    solve testProblem1.js

---
+++ problem1.js
@@ -0,0 +1,13 @@
+
+
+const sol1 = (inventory) =>{
+
+    for(let i = 0 ; i < inventory.length ; i++){
+        if(inventory[i].id === 33 ){
+            return `Car 33 is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model};`
+        }
+    }
+
+};
+
+module.exports =  sol1;
